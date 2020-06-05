4f3159bf6f0b8fcbed5e09789e17d8b8cbff6d3c
MrShip15 <mr_ship2001@mail.ru>
2020-06-05 16:28:25 +0300

    Fix codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,8 @@ int main()
 {
   std::string name;
   std::cin >> name;
-  std::cout << "Hello world from " << name << std::endl;  
+  std::cout << "Hello world from " << name
+                                   << std::endl;  
   return 0;
 
 }
