e6df3d02f39ead9b297eb0b6604d59c7c96eab5f
MrShip15 <mr_ship2001@mail.ru>
2020-06-05 16:48:59 +0300

    Style - Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-int main()
+int
+main()
 {
-  std::string name; //Users name
-  std::cin >> name; //Input users name
-  std::cout << "Hello world from " << name
-                                   << std::endl;  
+  std::string name; // Users name
+  std::cin >> name; // Input users name
+  std::cout << "Hello world from " << name << std::endl;
   return 0;
-
 }
